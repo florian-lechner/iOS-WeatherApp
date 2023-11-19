f0aed162229025ae5643ca27c060e413e77fda39
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-11-19 18:10:21 +0000

    Initial Commit

---
+++ MetUCD/MetUCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetUCDApp.swift
+//  MetUCD
+//
+//  Created by Flo Lechner on 19.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MetUCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
